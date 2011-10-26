d8e8eb2d4f76b653ccca31423ec138d0d1fd8acd
Pepe Doval <pepellou@gmail.com>
2011-10-26 20:37:38 +0200

    Main logic

--- estadisticas.php
+++ estadisticas.php
@@ -1,3 +1,13 @@
 <?php
 
+	include_once("pecados.php");
+
+	if (isset($_POST["sex"])) {
+		$sex = $_POST["sex"];
+		$questions = $_POST["id"];
+	
+		foreach ($questions as $id => $value) {
+		}
+	}
+
 ?>
--- pecadoscuestiones.php
+++ pecadoscuestiones.php
@@ -7,134 +7,22 @@
 
 <body>
 
-<form name="frm" action=procesa.php method="post">
+<form name="frm" action=estadisticas.php method="post">
 
-<h3>Pecados grandes a precios regulares</h3>
+<?php
+	include_once("pecados.php");
 
-<input type="submit" name="id[1]" value="1" /> He tomado drogas <br />
-<input type="checkbox" name="id[2]" value="2" />  ... y esto mas que una vez<br>
-<input type="checkbox" name="id[3]" value="3" /> He ofrecido drogas a alguien <br />
-<input type="checkbox" name="id[4]" value="4" /> He traficado drogas <br /> <br>
-
-<input type="checkbox" name="id[5]" value="5" /> A veces muestro el dedo del culo cuando estoy conduciendo <br />
-<input type="checkbox" name="id[6]" value="6" /> He causado un accidente <br />
-<input type="checkbox" name="id[7]" value="7" /> He golpeado otro coche cuando intentaba aparcar <br />
-<input type="checkbox" name="id[8]" value="8" />  ... y me he dado la fuga <br />
-<input type="checkbox" name="id[9]" value="9" /> He conducido ciegisimo <br /><br>
-
-<input type="checkbox" name="id[10]" value="10" /> A veces invento mentiras <br />
-<input type="checkbox" name="id[11]" value="11" /> Causo rumores <br />
-<input type="checkbox" name="id[12]" value="12" /> Hago acosos morales bastantes fuertes<br />
-<input type="checkbox" name="id[13]" value="13" /> A veces estoy despejellando sobre la fisica de los démas<br /><br>
-
-<input type="checkbox" name="id[14]" value="14" /> Me he despertado sin conocer la otra persona dormiendo en la misma cama  <br />
-<input type="checkbox" name="id[15]" value="15" /> He pagado por sexo <br />
-<input type="checkbox" name="id[16]" value="16" /> He cobrado por sexo <br />
-<input type="checkbox" name="id[17]" value="17" /> He tenido sexo en la cama de mis padres  <br /><br>
-
-<input type="checkbox" name="id[18]" value="18" /> He pegado alguien hasta tenia que irse al hospital <br />
-<input type="checkbox" name="id[19]" value="19" /> He robado bragas de la linea de otra gente <br />
-<input type="checkbox" name="id[20]" value="20" /> En momentos de descuido estoy miranod los cajones y armarios de otra gente <br /><br>
-
-<input type="checkbox" name="id[21]" value="21" /> He robado algo que vale mas que 100€ <br />
-<input type="checkbox" name="id[22]" value="22" /> He robado a mis padres  <br />
-<input type="checkbox" name="id[23]" value="23" /> He robado a mi pareja <br />
-<input type="checkbox" name="id[24]" value="24" /> He destruido algo solo por vandalismo <br />
-<input type="checkbox" name="id[25]" value="25" /> He pintado grafitis <br />
-<input type="checkbox" name="id[26]" value="26" /> No hago reciclaje <br /><br>
-
-<input type="checkbox" name="id[27]" value="27" /> He chupado a alguien/me ha chupado alguien conduciendo <br />
-<input type="checkbox" name="id[28]" value="28" /> He tenido sexo en la iglesia  <br />
-<input type="checkbox" name="id[29]" value="29" /> He tenido sexo cuando habia otra gente en la misma habitacion <br />
-<input type="checkbox" name="id[30]" value="30" /> He tenido un trio  <br /><br>
-
-<input type="checkbox" name="id[31]" value="31" /> He tenido sexo con alguien que tiene mas que 10 anyos mas que yo <br />
-<input type="checkbox" name="id[32]" value="32" /> He tenido sexo con alguien que tiene mas que 10 anyos menos que yo <br />
-<input type="checkbox" name="id[33]" value="33" /> He puesto los cuernos a alguien  <br />
-<input type="checkbox" name="id[34]" value="34" /> He tenido sexo al telefono o en el chat <br />
-<input type="checkbox" name="id[35]" value="35" /> He dicho a alguien que sea bueno en la cama sinque era la verdad  <br /><br>
-
-<input type="checkbox" name="id[36]" value="36" /> A veces aparco en el parking de disminuidos <br />
-<input type="checkbox" name="id[37]" value="37" />  ... und wurde dabei sogar schon abgeschleppt <br/>
-<input type="checkbox" name="id[38]" value="38" /> A veces voy en el autobus sin pagar<br />
-
-<input type="checkbox" name="id[39]" value="39" />  ... und wurde dabei schonmal erwischt  <br>
-<input type="checkbox" name="id[40]" value="40" />
-He saltado clases
-<br />
-<input type="checkbox" name="id[41]" value="41" />  ... und dabei noch den Entschuldigungszettel gef&auml;lscht 
-<p>
-  <input type="checkbox" name="id[42]" value="42" /> 
-  A veces ni hice mis deberes<br />
-  <input type="checkbox" name="id[43]" value="43" /> 
-  He escondido notas malas de mis padres<br />
-  
-  <input type="checkbox" name="id[44]" value="44" />
-  He suspendido una clase
-  <br /><br>
-  <input type="checkbox" name="id[45]" value="45" />
-  Mi pelo es tintado
-</p>
-<p>
-  <input type="checkbox" name="id[46]" value="46" /> 
-  Mis tetas son falsas<br />
-  <input type="checkbox" name="id[47]" value="47" /> 
-  Miento sobre mi peso<br />
-  <input type="checkbox" name="id[48]" value="48" /> 
-  He grabado un video sucio/porno<br />
-  
-  <input type="checkbox" name="id[49]" value="49" /> 
-  He tenido ciber-sexo o sexo al telefono<br /><br>
-  <br>
-</p>
-<h3>Pecados pequenos en rebaja</h3><input type="checkbox" name="id[51]" value="51" /> 
-Tiro pedos y doy la culpa a otra gente <br />
-<input type="checkbox" name="id[52]" value="52" /> 
-Miento sobre mi genero en el chat <br />
-<input type="checkbox" name="id[53]" value="53" /> 
-He robado frutas <br />
-
-<input type="checkbox" name="id[54]" value="54" /> 
-Hoy ya he mentido  <br /><br>
-<input type="checkbox" name="id[55]" value="55" /> 
-Hoy ya he masturbado  <br />
-<input type="checkbox" name="id[56]" value="56" />  ... und mir hinterher nicht die H&auml;nde gewaschen 
-<input type="checkbox" name="id[57]" value="57" /> 
-A veces uso juguetes de sexo <br />
-<input type="checkbox" name="id[58]" value="58" /> 
-Me gusta sexo anal <br />
-<input type="checkbox" name="id[59]" value="59" /> 
-Me gusta sexo oral <br /><br>
-
-<input type="checkbox" name="id[60]" value="60" /> 
-Me gusta jugar gira-botellas <br />
-<input type="checkbox" name="id[61]" value="61" /> 
-Me meo en la piscina <br />
-<input type="checkbox" name="id[62]" value="62" />  ... w&uuml;rde das aber niemals zugeben <br>
-<input type="checkbox" name="id[63]" value="63" /> 
-Me gusta sexo en piscinas publicas <br />
-<input type="checkbox" name="id[64]" value="64" /> 
-He tenido sexo en el bosque o a la playa <br />
-
-<input type="checkbox" name="id[65]" value="65" /> 
-He besado alguien con el mismo genero <br />
-<input type="checkbox" name="id[66]" value="66" /> 
-He besado mi profesor/a <br />
-<input type="checkbox" name="id[67]" value="67" /> 
-Me he acostado con alguien del mismo genero <br />
-<input type="hidden" name="cnt" value="0" /><br>
-
+	foreach ($pecados as $id => $pecado) {
+		echo "<input type=checkbox name='id[$id]' value='$id' />$pecado<br />";
+	}
+?>
 <h3>Y algunos datos mas personales...</h3>
-Como es tu nombre (sobrenombre o usuario es suficiente)?<br>
-<input name="name" size="40" maxlength="60" value="" />
-<br><br>
-De que genero eres?<br>
+De qu&eacute; g&eacute;nero eres?<br>
 <input type="radio" name="sex" value="m" checked />
 masculino&nbsp;&nbsp;
 <input type="radio" name="sex" value="w" />
 feminino&nbsp;&nbsp; <br><br>
 <input type="submit" value="evaluar" /> 
-<font color="#FF0000">&nbsp;* No olvides nombre y genero</font>
 
 </form>  <br><br>
 
