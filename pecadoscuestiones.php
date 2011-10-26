4863fdade4d3806fe3ca23c982f06af83563f48f
Pepe Doval <pepellou@gmail.com>
2011-10-26 10:02:40 +0200

    Clean

--- pecadoscuestiones.php
+++ pecadoscuestiones.php
@@ -1,204 +1,128 @@
-</html>
-<?xml version="1.0" encoding="utf-8" ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<title>S&uuml;ndenrechner | Deine S&uuml;nden bitte!</title>
-<meta name="generator" content="Webweaver v1.6.7 [http://webweaver.ws]" />
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-var pageTracker = _gat._getTracker("UA-3444863-1");
-pageTracker._initData();
-pageTracker._trackPageview();
-</script>
-<script type="text/javascript">
-var myval=0;
-function showDiv (name) {
-myval++;
-document.frm.cnt.value = myval;
-if(document.getElementById(name).style.display=="none"){
-//document.getElementById(name).style.display = "block";
-
-  Effect.SlideDown(document.getElementById(name), {duration:0.2});
-  new Effect.Highlight(document.getElementById(name), {startcolor:'#FF9999', endcolor:'#FFFFFF'});
-}
-else Effect.SlideUp(document.getElementById(name), {duration:0.2});
-}
-</script>
-<script src="inc/lib/prototype.js" type="text/javascript"></script>
-<script src="inc/src/scriptaculous.js" type="text/javascript"></script>
+	<title>Calculadora de pecados</title>
+	<link rel=stylesheet type="text/css" href="./css/style.css">
 </head>
 
 <body>
-<link rel=stylesheet type="text/css" href="./inc/style.css">
 
- <div align="center">
- <form name="frm" action="end.php" method="post">
-<table id="tra">
- <tr>
-  <td>
+<form name="frm" action="" method="post">
 
-<h3>Pecados grandes a precios regulares</h3><input onClick="showDiv('off1')" type="checkbox" name="id[1]" value="1" /> 
-He tomado drogas <br />
+<h3>Pecados grandes a precios regulares</h3>
 
-<div id="off1" style="display:none;"><input onClick="showDiv('off2')" type="checkbox" name="id[2]" value="2" />  ... y esto mas que una vez </div><br>
-<input onClick="showDiv('off3')" type="checkbox" name="id[3]" value="3" /> 
-He ofrecido drogas a alguien <br />
-<input onClick="showDiv('off4')" type="checkbox" name="id[4]" value="4" /> 
-He traficado drogas <br />
-<br>
-<input onClick="showDiv('off5')" type="checkbox" name="id[5]" value="5" /> 
-A veces muestro el dedo del culo cuando estoy conduciendo <br />
-<input onClick="showDiv('off6')" type="checkbox" name="id[6]" value="6" /> 
-He causado un accidente <br />
-<input onClick="showDiv('off7')" type="checkbox" name="id[7]" value="7" /> 
-He golpeado otro coche cuando intentaba aparcar <br />
+<input type="submit" name="id[1]" value="1" /> He tomado drogas <br />
+<input type="checkbox" name="id[2]" value="2" />  ... y esto mas que una vez<br>
+<input type="checkbox" name="id[3]" value="3" /> He ofrecido drogas a alguien <br />
+<input type="checkbox" name="id[4]" value="4" /> He traficado drogas <br /> <br>
 
-<div id="off7" style="display:none;"><input onClick="showDiv('off8')" type="checkbox" name="id[8]" value="8" />  ... y me he dado la fuga </div>
-<input onClick="showDiv('off9')" type="checkbox" name="id[9]" value="9" /> 
-He conducido ciegisimo <br /><br>
-<input onClick="showDiv('off10')" type="checkbox" name="id[10]" value="10" /> 
-A veces invento mentiras <br />
-<input onClick="showDiv('off11')" type="checkbox" name="id[11]" value="11" /> 
-Causo rumores <br />
-<input onClick="showDiv('off12')" type="checkbox" name="id[12]" value="12" /> 
-Hago acosos morales bastantes fuertes<br />
+<input type="checkbox" name="id[5]" value="5" /> A veces muestro el dedo del culo cuando estoy conduciendo <br />
+<input type="checkbox" name="id[6]" value="6" /> He causado un accidente <br />
+<input type="checkbox" name="id[7]" value="7" /> He golpeado otro coche cuando intentaba aparcar <br />
+<input type="checkbox" name="id[8]" value="8" />  ... y me he dado la fuga <br />
+<input type="checkbox" name="id[9]" value="9" /> He conducido ciegisimo <br /><br>
 
-<input onClick="showDiv('off13')" type="checkbox" name="id[13]" value="13" /> 
-A veces estoy despejellando sobre la fisica de los démas<br /><br>
-<input onClick="showDiv('off14')" type="checkbox" name="id[14]" value="14" /> 
-Me he despertado sin conocer la otra persona dormiendo en la misma cama  <br />
-<input onClick="showDiv('off15')" type="checkbox" name="id[15]" value="15" /> 
-He pagado por sexo <br />
-<input onClick="showDiv('off16')" type="checkbox" name="id[16]" value="16" /> 
-He cobrado por sexo <br />
-<input onClick="showDiv('off17')" type="checkbox" name="id[17]" value="17" /> 
-He tenido sexo en la cama de mis padres  <br /><br>
+<input type="checkbox" name="id[10]" value="10" /> A veces invento mentiras <br />
+<input type="checkbox" name="id[11]" value="11" /> Causo rumores <br />
+<input type="checkbox" name="id[12]" value="12" /> Hago acosos morales bastantes fuertes<br />
+<input type="checkbox" name="id[13]" value="13" /> A veces estoy despejellando sobre la fisica de los démas<br /><br>
 
-<input onClick="showDiv('off18')" type="checkbox" name="id[18]" value="18" /> 
-He pegado alguien hasta tenia que irse al hospital <br />
-<input onClick="showDiv('off19')" type="checkbox" name="id[19]" value="19" /> 
-He robado bragas de la linea de otra gente <br />
-<input onClick="showDiv('off20')" type="checkbox" name="id[20]" value="20" /> 
-En momentos de descuido estoy miranod los cajones y armarios de otra gente <br /><br>
-<input onClick="showDiv('off21')" type="checkbox" name="id[21]" value="21" /> 
-He robado algo que vale mas que 100€ <br />
-<input onClick="showDiv('off22')" type="checkbox" name="id[22]" value="22" /> 
-He robado a mis padres  <br />
+<input type="checkbox" name="id[14]" value="14" /> Me he despertado sin conocer la otra persona dormiendo en la misma cama  <br />
+<input type="checkbox" name="id[15]" value="15" /> He pagado por sexo <br />
+<input type="checkbox" name="id[16]" value="16" /> He cobrado por sexo <br />
+<input type="checkbox" name="id[17]" value="17" /> He tenido sexo en la cama de mis padres  <br /><br>
 
-<input onClick="showDiv('off23')" type="checkbox" name="id[23]" value="23" /> 
-He robado a mi pareja <br />
-<input onClick="showDiv('off24')" type="checkbox" name="id[24]" value="24" /> 
-He destruido algo solo por vandalismo <br />
-<input onClick="showDiv('off25')" type="checkbox" name="id[25]" value="25" /> 
-He pintado grafitis <br />
-<input onClick="showDiv('off26')" type="checkbox" name="id[26]" value="26" />
-No hago reciclaje
-<br /><br>
-<input onClick="showDiv('off27')" type="checkbox" name="id[27]" value="27" /> 
-He chupado a alguien/me ha chupado alguien conduciendo <br />
+<input type="checkbox" name="id[18]" value="18" /> He pegado alguien hasta tenia que irse al hospital <br />
+<input type="checkbox" name="id[19]" value="19" /> He robado bragas de la linea de otra gente <br />
+<input type="checkbox" name="id[20]" value="20" /> En momentos de descuido estoy miranod los cajones y armarios de otra gente <br /><br>
 
-<input onClick="showDiv('off28')" type="checkbox" name="id[28]" value="28" /> 
-He tenido sexo en la iglesia  <br />
-<input onClick="showDiv('off29')" type="checkbox" name="id[29]" value="29" />
-He tenido sexo cuando habia otra gente en la misma habitacion <br />
-<input onClick="showDiv('off30')" type="checkbox" name="id[30]" value="30" /> 
-He tenido un trio  <br /><br>
-<input onClick="showDiv('off31')" type="checkbox" name="id[31]" value="31" /> 
-He tenido sexo con alguien que tiene mas que 10 anyos mas que yo <br />
-<input onClick="showDiv('off32')" type="checkbox" name="id[32]" value="32" /> 
-He tenido sexo con alguien que tiene mas que 10 anyos menos que yo <br />
+<input type="checkbox" name="id[21]" value="21" /> He robado algo que vale mas que 100€ <br />
+<input type="checkbox" name="id[22]" value="22" /> He robado a mis padres  <br />
+<input type="checkbox" name="id[23]" value="23" /> He robado a mi pareja <br />
+<input type="checkbox" name="id[24]" value="24" /> He destruido algo solo por vandalismo <br />
+<input type="checkbox" name="id[25]" value="25" /> He pintado grafitis <br />
+<input type="checkbox" name="id[26]" value="26" /> No hago reciclaje <br /><br>
 
-<input onClick="showDiv('off33')" type="checkbox" name="id[33]" value="33" /> 
-He puesto los cuernos a alguien  <br />
-<input onClick="showDiv('off34')" type="checkbox" name="id[34]" value="34" /> 
-He tenido sexo al telefono o en el chat <br />
-<input onClick="showDiv('off35')" type="checkbox" name="id[35]" value="35" /> 
-He dicho a alguien que sea bueno en la cama sinque era la verdad  <br /><br>
-<input onClick="showDiv('off36')" type="checkbox" name="id[36]" value="36" /> 
-A veces aparco en el parking de disminuidos <br />
-<div id="off36" style="display:none;"><input onClick="showDiv('off37')" type="checkbox" name="id[37]" value="37" />  ... und wurde dabei sogar schon abgeschleppt </div>
-<input onClick="showDiv('off38')" type="checkbox" name="id[38]" value="38" /> 
-A veces voy en el autobus sin pagar<br />
+<input type="checkbox" name="id[27]" value="27" /> He chupado a alguien/me ha chupado alguien conduciendo <br />
+<input type="checkbox" name="id[28]" value="28" /> He tenido sexo en la iglesia  <br />
+<input type="checkbox" name="id[29]" value="29" /> He tenido sexo cuando habia otra gente en la misma habitacion <br />
+<input type="checkbox" name="id[30]" value="30" /> He tenido un trio  <br /><br>
 
-<div id="off38" style="display:none;"><input onClick="showDiv('off39')" type="checkbox" name="id[39]" value="39" />  ... und wurde dabei schonmal erwischt  </div><br>
-<input onClick="showDiv('off40')" type="checkbox" name="id[40]" value="40" />
+<input type="checkbox" name="id[31]" value="31" /> He tenido sexo con alguien que tiene mas que 10 anyos mas que yo <br />
+<input type="checkbox" name="id[32]" value="32" /> He tenido sexo con alguien que tiene mas que 10 anyos menos que yo <br />
+<input type="checkbox" name="id[33]" value="33" /> He puesto los cuernos a alguien  <br />
+<input type="checkbox" name="id[34]" value="34" /> He tenido sexo al telefono o en el chat <br />
+<input type="checkbox" name="id[35]" value="35" /> He dicho a alguien que sea bueno en la cama sinque era la verdad  <br /><br>
+
+<input type="checkbox" name="id[36]" value="36" /> A veces aparco en el parking de disminuidos <br />
+<input type="checkbox" name="id[37]" value="37" />  ... und wurde dabei sogar schon abgeschleppt <br/>
+<input type="checkbox" name="id[38]" value="38" /> A veces voy en el autobus sin pagar<br />
+
+<input type="checkbox" name="id[39]" value="39" />  ... und wurde dabei schonmal erwischt  <br>
+<input type="checkbox" name="id[40]" value="40" />
 He saltado clases
 <br />
-<div id="off40" style="display:none;"><input onClick="showDiv('off41')" type="checkbox" name="id[41]" value="41" />  ... und dabei noch den Entschuldigungszettel gef&auml;lscht </div>
+<input type="checkbox" name="id[41]" value="41" />  ... und dabei noch den Entschuldigungszettel gef&auml;lscht 
 <p>
-  <input onClick="showDiv('off42')" type="checkbox" name="id[42]" value="42" /> 
+  <input type="checkbox" name="id[42]" value="42" /> 
   A veces ni hice mis deberes<br />
-  <input onClick="showDiv('off43')" type="checkbox" name="id[43]" value="43" /> 
+  <input type="checkbox" name="id[43]" value="43" /> 
   He escondido notas malas de mis padres<br />
   
-  <input onClick="showDiv('off44')" type="checkbox" name="id[44]" value="44" />
+  <input type="checkbox" name="id[44]" value="44" />
   He suspendido una clase
   <br /><br>
-  <input onClick="showDiv('off45')" type="checkbox" name="id[45]" value="45" />
+  <input type="checkbox" name="id[45]" value="45" />
   Mi pelo es tintado
 </p>
 <p>
-  <input onClick="showDiv('off46')" type="checkbox" name="id[46]" value="46" /> 
+  <input type="checkbox" name="id[46]" value="46" /> 
   Mis tetas son falsas<br />
-  <input onClick="showDiv('off47')" type="checkbox" name="id[47]" value="47" /> 
+  <input type="checkbox" name="id[47]" value="47" /> 
   Miento sobre mi peso<br />
-  <input onClick="showDiv('off48')" type="checkbox" name="id[48]" value="48" /> 
+  <input type="checkbox" name="id[48]" value="48" /> 
   He grabado un video sucio/porno<br />
   
-  <input onClick="showDiv('off49')" type="checkbox" name="id[49]" value="49" /> 
+  <input type="checkbox" name="id[49]" value="49" /> 
   He tenido ciber-sexo o sexo al telefono<br /><br>
   <br>
 </p>
-<h3>Pecados pequenos en rebaja</h3><input onClick="showDiv('off51')" type="checkbox" name="id[51]" value="51" /> 
+<h3>Pecados pequenos en rebaja</h3><input type="checkbox" name="id[51]" value="51" /> 
 Tiro pedos y doy la culpa a otra gente <br />
-<input onClick="showDiv('off52')" type="checkbox" name="id[52]" value="52" /> 
+<input type="checkbox" name="id[52]" value="52" /> 
 Miento sobre mi genero en el chat <br />
-<input onClick="showDiv('off53')" type="checkbox" name="id[53]" value="53" /> 
+<input type="checkbox" name="id[53]" value="53" /> 
 He robado frutas <br />
 
-<input onClick="showDiv('off54')" type="checkbox" name="id[54]" value="54" /> 
+<input type="checkbox" name="id[54]" value="54" /> 
 Hoy ya he mentido  <br /><br>
-<input onClick="showDiv('off55')" type="checkbox" name="id[55]" value="55" /> 
+<input type="checkbox" name="id[55]" value="55" /> 
 Hoy ya he masturbado  <br />
-<div id="off55" style="display:none;"><input onClick="showDiv('off56')" type="checkbox" name="id[56]" value="56" />  ... und mir hinterher nicht die H&auml;nde gewaschen </div>
-<input onClick="showDiv('off57')" type="checkbox" name="id[57]" value="57" /> 
+<input type="checkbox" name="id[56]" value="56" />  ... und mir hinterher nicht die H&auml;nde gewaschen 
+<input type="checkbox" name="id[57]" value="57" /> 
 A veces uso juguetes de sexo <br />
-<input onClick="showDiv('off58')" type="checkbox" name="id[58]" value="58" /> 
+<input type="checkbox" name="id[58]" value="58" /> 
 Me gusta sexo anal <br />
-<input onClick="showDiv('off59')" type="checkbox" name="id[59]" value="59" /> 
+<input type="checkbox" name="id[59]" value="59" /> 
 Me gusta sexo oral <br /><br>
 
-<input onClick="showDiv('off60')" type="checkbox" name="id[60]" value="60" /> 
+<input type="checkbox" name="id[60]" value="60" /> 
 Me gusta jugar gira-botellas <br />
-<input onClick="showDiv('off61')" type="checkbox" name="id[61]" value="61" /> 
+<input type="checkbox" name="id[61]" value="61" /> 
 Me meo en la piscina <br />
-<div id="off61" style="display:none;"><input onClick="showDiv('off62')" type="checkbox" name="id[62]" value="62" />  ... w&uuml;rde das aber niemals zugeben </div><br>
-<input onClick="showDiv('off63')" type="checkbox" name="id[63]" value="63" /> 
+<input type="checkbox" name="id[62]" value="62" />  ... w&uuml;rde das aber niemals zugeben <br>
+<input type="checkbox" name="id[63]" value="63" /> 
 Me gusta sexo en piscinas publicas <br />
-<input onClick="showDiv('off64')" type="checkbox" name="id[64]" value="64" /> 
+<input type="checkbox" name="id[64]" value="64" /> 
 He tenido sexo en el bosque o a la playa <br />
 
-<input onClick="showDiv('off65')" type="checkbox" name="id[65]" value="65" /> 
+<input type="checkbox" name="id[65]" value="65" /> 
 He besado alguien con el mismo genero <br />
-<input onClick="showDiv('off66')" type="checkbox" name="id[66]" value="66" /> 
+<input type="checkbox" name="id[66]" value="66" /> 
 He besado mi profesor/a <br />
-<input onClick="showDiv('off67')" type="checkbox" name="id[67]" value="67" /> 
+<input type="checkbox" name="id[67]" value="67" /> 
 Me he acostado con alguien del mismo genero <br />
 <input type="hidden" name="cnt" value="0" /><br>
-<center><!--Banner goes here--> <script type="text/javascript"><!--
-google_ad_client = "pub-8122320310520638";
-/* 728x90, Erstellt 01.12.09 */
-google_ad_slot = "9134253232";
-google_ad_width = 728;
-google_ad_height = 90;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>  <!--End of Banner--></center>
 
 <h3>Y algunos datos mas personales...</h3>
 Como es tu nombre (sobrenombre o usuario es suficiente)?<br>
@@ -213,10 +137,6 @@ feminino&nbsp;&nbsp; <br><br>
 <font color="#FF0000">&nbsp;* No olvides nombre y genero</font>
 
 </form>  <br><br>
-    </td>
- </tr>
-</table>
-</center>
 
 
 </body>
