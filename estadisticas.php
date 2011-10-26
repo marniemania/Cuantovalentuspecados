9994b035ce53b358da8fc4386557a4f8cb467ac2
Pepe Doval <pepellou@gmail.com>
2011-10-26 21:38:13 +0200

    Files for storing pecados

--- estadisticas.php
+++ estadisticas.php
@@ -10,4 +10,30 @@
 		}
 	}
 
+	function get_values($file) {
+		$results = array();
+		$contents = file_get_contents($file);
+		for ($pos = 0; $pos < strlen($contents); $pos++) {
+			if ($pos %2 == 0)
+				$results []= substr($contents, $pos, 1);
+		}
+		return $results;
+	}
+
+	$hombres = get_values("pecadosm.txt");
+	$mujeres = get_values("pecadosf.txt");
+
+	echo "<table border=1>";
+	echo "<tr><th>Pecado</th><th>Hombres</th><th>Mujeres</th></tr>";
+	foreach ($hombres as $id => $count) {
+		echo "<tr>";
+		echo "<td>"; echo $pecados[$id]; echo "</td>";
+		echo "<td>"; echo $count; echo "</td>";
+		echo "<td>"; echo $mujeres[$id]; echo "</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+
 ?>
+
+<a href=index.html>Volver a empezar</a>
