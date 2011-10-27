6b6c1890c7534e9ef00e6d70609416f8f1522bb0
Pepe Doval <pepellou@gmail.com>
2011-10-27 20:03:32 +0200

    Refactor

--- pecados.php
+++ pecados.php
@@ -1,9 +1,30 @@
 <?php
 
-	$pecados = array(
-		"He meado a alguien",
-		"Hoy he mentido",
-		"Me gusta el sexo oral"
-	);
+class Pecado {
+
+	var $text;
+
+	private function __construct(
+		$text
+	) {
+		$this->text = $text;
+	}
+
+	public function text(
+	) {
+		return $this->text;
+	}
+
+	public static function all(
+	) {
+		return array(
+			new Pecado("He meado a alguien"),
+			new Pecado("Hoy he mentido"),
+			new Pecado("Me gusta el sexo oral")
+		);
+	}
+
+}
+
 
 ?>
